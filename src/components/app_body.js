ad1b4fa0ba39c4a9aa3dad9fb8494c6206462973
Mateo <madiazt@unal.edu.co>
2021-06-05 16:56:24 -0500

    added form and new edu data logic

--- src/components/app_body.js
+++ src/components/app_body.js
@@ -1,12 +1,13 @@
 import React from "react"
 import PersonalData from "./data_components/PersonalData"
 import ExperienceData from "./data_components/ExperienceData"
+import EducationData from "./data_components/EducationData"
 import uniqid from "uniqid";
 
 const AppBody = () => {
     const [personalData, set_personalData] = React.useState({})
     const [expData, set_expData] = React.useState([])
-    
+    const [eduData, set_eduData] = React.useState([])
 
     const handle_PersonalData = (data) => {
         set_personalData(data)
@@ -33,10 +34,27 @@ const AppBody = () => {
         let new_object ={key:id}
         set_expData(expData.concat(new_object))
     }
+
+    const handle_eduData = (data) => {
+        //todo
+        
+    }
+
+    const removing_eduData = (dataKey) => {
+        //todo
+    }
+
+    const new_eduData = () => {
+        //TEST
+        let id = uniqid()
+        let new_object = {key:id}
+        set_eduData(eduData.concat(new_object))
+    }
+
     const userData = {
         p_data: personalData,
         edu_data: expData,
-        exp_data: [],
+        exp_data: eduData,
     }
     function form_exp_data() {
         let elements = expData.map(exp_object => {
@@ -47,6 +65,19 @@ const AppBody = () => {
                 deleteData={removing_expData}/>
             )
             }
+        )
+        return elements
+    }
+
+    const form_edu_data = () => {
+        //Test
+        let elements = eduData.map(edu_object => {
+            return(
+                <EducationData key={edu_object.key} id={edu_object.key}
+                getData={handle_eduData}
+                deleteData={removing_eduData} />
+            )
+        }
         )
         return elements
     }
