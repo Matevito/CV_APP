862dd03ca3065054130a65df37dbf4e2a767d1da
Mateo <madiazt@unal.edu.co>
2021-06-05 16:16:42 -0500

    added handle edu stated variables

--- src/components/data_components/EducationData.js
+++ src/components/data_components/EducationData.js
@@ -1,7 +1,44 @@
 import React from "react"
 
-const EducationData = () => {
+const EducationData = (props) => {
     //code goes here
+    const [title, set_title] = React.useState("");
+    const [organization, set_organization] = React.useState("");
+    const [fromDate, set_fromDate] = React.useState("");
+    const [toDate, set_toDate] = React.useState("");
+
+    const eduData = {
+        key: props.id,
+
+        title: title,
+        organization: organization,
+        fromDate: fromDate,
+        toDate: toDate,
+    }
+    const handle_title = (event) => {
+        set_title(event.target.value)
+    }
+    const handle_organization = (event) => {
+        set_organization(event.target.value)
+    }
+    const handle_fromDate = (event) => {
+        set_fromDate(event.target.value)
+    }
+    const handle_toDate = (event) => {
+        set_toDate(event.target.value)
+    }
+
+    const save_data = () => {
+        props.getData(eduData)
+    }
+    const delete_data = () => {
+        props.deleteData(eduData.key)
+    }
+    return (
+        <div>
+            
+        </div>
+    )
 }
 
 export default EducationData
